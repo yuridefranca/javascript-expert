571f6dd8114d47652e5139fa1e587a443440a4cf
Yuri Serafim <yuridefranca@gmail.com>
2023-03-07 22:24:22 -0300

    ci: criado estrutura para o projeto

---
+++ startDocker.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+FILE=./.env
+if [ ! -f "$FILE" ]; then
+    cp .env.example .env
+fi
+
+docker-compose up --build -d
+
+# docker-compose logs -f
+docker-compose exec app bash
